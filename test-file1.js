aff63b3577b0d44ba9208de7534eda6adc26372f
xgqfrms <xgqfrms@outlook.com>
2020-07-19 11:55:56 +0800

    Create test-file1.js

---
+++ test-file1.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-07-18
+ * @modified
+ *
+ * @description 
+ * @difficulty Easy
+ * @complexity O(n)
+ * @augments
+ * @example
+ * @link
+ * @solutions
+ *
+ */
+
+const log = console.log;
+
+log(`test-file1.js`);
